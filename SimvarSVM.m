d3b0c5a875c6f9a594fe6e38beb20ca5e9b2726f
mysablehats <frederico.klein@gmail.com>
2017-10-03 11:02:52 +0100

    make it able to run in parallel, since we are no longer in the initial testing..

--- SimvarSVM.m
+++ SimvarSVM.m
@@ -16,7 +16,7 @@ classdef SimvarSVM < Simvar
             %% Post initialization
             simvar.method = 'svm';
             simvar.excfun = @(data,ii)executesvm(simvar(ii).pars, data); %if pars is empty, this will not work!
-            simvar.PARA = 0; % im testing now
+            %simvar.PARA = 0; % im testing now
             
             
         end
